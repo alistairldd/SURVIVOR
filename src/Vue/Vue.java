8a8495e54ecfd5e30817e0f6965fe20128ae4f90
alistairldd <150132411+alistairldd@users.noreply.github.com>
2026-02-09 16:49:15 +0100

    Add files via upload

--- src/Vue/Vue.java
+++ src/Vue/Vue.java
@@ -1,4 +1,11 @@
 package Vue;
 
+/*
+* La classe générale de la vue, elle contient les classes de données et les méthodes pour manipuler ces données.
+* Elle est utilisée pour afficher les données de l'application et pour recevoir les événements de l'utilisateur
+* et pour les transmettre au contrôleur. Elle est également utilisée pour gérer les threads de la vue.
+*
+ */
+
 public class Vue {
 }
