1a5e3566c7a68a6dca7bcc4521724913c8e6c0eb
marc.wang <marc.wang@universite-paris-saclay.fr>
2026-03-30 16:50:11 +0200

    VueRessource images

--- src/Vue/VueRessource.java
+++ src/Vue/VueRessource.java
@@ -1,16 +1,33 @@
 package Vue;
 
 import Modele.Ressource;
+
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Utilitaires de dessin pour les ressources tombées au sol.
  * Associe visuellement un ID de matériau (Modèle) à une couleur (Vue).
  */
 public class VueRessource {
+    private BufferedImage imageFer;
+    private BufferedImage imageOr;
+    private BufferedImage imagePierre;
 
     public VueRessource() {
-        // Le constructeur est vide maintenant car on ne stocke plus de position unique ici
+        // Importe les images des ressources
+        try {
+            this.imageFer = ImageIO.read(new File("src/images/lingot_fer.png"));
+            this.imageOr = ImageIO.read(new File("src/images/lingot_or.png"));
+            this.imagePierre = ImageIO.read(new File("src/images/pierre.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Erreur : Impossible de charger l'image.");
+        }
+
     }
 
     // MODIFICATION : On ajoute les paramètres 'Ressource r', 'x', et 'y'
@@ -22,36 +39,44 @@ public class VueRessource {
      * @param y Coordonnée absolue ou mise à l'échelle.
      * @param minimap Définit si l'objet doit être miniaturisé.
      */
-    public static void dessinerRessource(Graphics g, Ressource r, int x, int y, boolean minimap) {
+    public void dessinerRessource(Graphics g, Ressource r, int x, int y, boolean minimap) {
         // Dictionnaire visuel : associe un type (0, 1, 2, 3) à un code couleur précis (RGB)
         // 0 (Bois) -> Marron
         // 1 (Pierre) -> Gris clair
         // 2 (Fer) -> Gris argent/métallique
         // 3 (Or) -> Jaune/Doré
-        Color[] col = {new Color(109,71,49), new Color(123,123,125),  new Color(182, 182, 182), new Color(218, 165, 32)}; // marron, gris, jaune
 
         // Diamètre de l'objet sur la carte
-        int taille = 20;
+        int taille = 50;
 
         if (minimap){
             // Si on dessine sur la minimap, on réduit la taille des ressources à de minuscules points de 4 pixels
-            taille = 4;
+            taille = 16;
         }
 
         // Récupère l'identifiant du matériau
         int type = r.getType();
-
-        // Sécurité pour la couleur : empêche un crash "ArrayIndexOutOfBounds" si une nouvelle ressource non prévue est ajoutée au Modèle
-        if (type >= 0 && type < col.length) {
-            // Applique la couleur correspondante issue du tableau
-            g.setColor(col[type]);
-        } else {
-            // Couleur de secours en cas d'erreur
-            g.setColor(Color.BLACK);
+        // On dessine aux coordonnées x, y fournies par la Vue principale (qui a déjà appliqué la translation de Caméra)
+        switch (type) {
+            case 0: // Bois
+                g.setColor(new Color(139, 69, 19)); // Marron
+                g.fillOval(x, y, taille, taille);
+                break;
+            case 1: // Pierre
+                g.drawImage(imagePierre, x, y, taille, taille, null);
+                break;
+            case 2: // Fer
+                g.drawImage(imageFer, x, y, taille, taille, null);
+                break;
+            case 3: // Or
+                g.drawImage(imageOr, x, y, taille, taille, null);
+                break;
+            default:
+                g.setColor(Color.BLACK); // Couleur de secours
+                g.fillOval(x, y, taille, taille);
         }
 
-        // On dessine aux coordonnées x, y fournies par la Vue principale (qui a déjà appliqué la translation de Caméra)
-        // La forme est un simple cercle (Oval de largeur égale à hauteur)
-        g.fillOval(x, y, taille, taille);
+
+
     }
 }
